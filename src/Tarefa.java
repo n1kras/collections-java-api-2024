30f03bc503faae9528cdcce62b67a8ae28c617ac
n1kras <mariano.nicolaso@gmail.com>
2024-09-25 16:24:59 -0400

    aprendendo a operar uma List

---
+++ src/Tarefa.java
@@ -0,0 +1,10 @@
+public class Tarefa {
+    private String descricao;
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
