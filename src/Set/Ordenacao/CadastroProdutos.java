f9c6a35cc95c81590cdc6bd3bcc50afd1817a691
n1kras <mariano.nicolaso@gmail.com>
2024-09-26 10:51:09 -0400

    Testando as operações de Ordenação de Set com Comparable e Comparator.

---
+++ src/Set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,28 @@
+package Set.Ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int qtd) {
+        produtoSet.add(new Produto(cod, nome, preco, qtd));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirProdutosPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+}
