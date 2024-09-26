72746b8950ac20bdcd20a0542d6e25c958ab5bb2
n1kras <mariano.nicolaso@gmail.com>
2024-09-26 11:50:54 -0400

    Aprendendo as operações Básicas de Map.

--- src/README.md
+++ src/README.md
@@ -1,3 +1,4 @@
 # List, Set e Map #
 List: pode ter elementos duplicaados
 Set: nao pode conter elementos duplicados, implementa-se o equals e o hashset para definir qual atributo da classe define que é unico
+Map: usada para mapear dados na forma de chaves e valores. Nao pode conter chaves duplicadas.
--- src/Set/Ordenacao/CadastroProdutos.java
+++ src/Set/Ordenacao/CadastroProdutos.java
@@ -25,4 +25,19 @@ public class CadastroProdutos {
         produtosPorPreco.addAll(produtoSet);
         return produtosPorPreco;
     }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        cadastroProdutos.adicionarProduto(1L, "Produto 5", 15d, 5);
+        cadastroProdutos.adicionarProduto(2L, "Produto 0", 20d, 10);
+        cadastroProdutos.adicionarProduto(1L, "Produto 3", 10d, 2);
+        cadastroProdutos.adicionarProduto(9L, "Produto 9", 2d, 2);
+
+       // System.out.println(cadastroProdutos.produtoSet); // exibe somente 3 pois produtos com codigos iguais nao sao permitidos
+
+       // System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
 }
