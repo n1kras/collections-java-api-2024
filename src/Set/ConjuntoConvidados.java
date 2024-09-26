dcc89dc12b961b777c04cd105f54e9158c2bc1cc
n1kras <mariano.nicolaso@gmail.com>
2024-09-26 09:59:41 -0400

    Aprendendo as operações básicas de Set (adicionar, remover). Entendendo a diferença do List e do Set

---
+++ src/Set/ConjuntoConvidados.java
@@ -0,0 +1,27 @@
+package Set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite) {
+        Convidado convidadoARemover = null;
+        for (Convidado c : convidadoSet) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoARemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoARemover);
+    }
+}
---
+++ src/Set/README.md
@@ -0,0 +1,3 @@
+# List, Set e Map #
+List: pode ter elementos duplicaados
+Set: nao pode conter elementos duplicados
