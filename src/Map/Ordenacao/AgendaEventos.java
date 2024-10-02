93cff5ec79755c9b98558e7b8ee8307039730d26
n1kras <mariano.nicolaso@gmail.com>
2024-10-01 20:53:24 -0400

    Aprendendo as operacoes de Ordenacao de Map.

--- src/Map/Ordenacao/AgendaEventos.java
+++ src/Map/Ordenacao/AgendaEventos.java
@@ -1,10 +1,7 @@
 package Map.Ordenacao;
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class AgendaEventos {
     private Map<LocalDate,Evento> agendaEventoMap;
@@ -20,6 +17,21 @@ public class AgendaEventos {
     public void exibirAgenda() {
         Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(agendaEventoMap);
         System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento() {
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventoTreeMap = new TreeMap<>(agendaEventoMap);
+        for (Map.Entry<LocalDate, Evento> entry : agendaEventoMap.entrySet()) {
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O próximo evento: " + proximoEvento + " acontecerá na data " + proximaData);
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
